c0e34dbcec9a0886ca46bd194cff0e87c2d00479
Aesonus <corylcomposinger@gmail.com>
2020-12-11 09:42:59 -0600

    Changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="tklife", # Replace with your own username
-    version="0.2.3",
+    version="0.3.0",
     author="Cory Laughlin",
     author_email="corylcomposinger@gmail.com",
     description="COntains some basic classes for Tkinter Applications",
