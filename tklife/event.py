d1f09b9990cf6e4d402d384b36622999bfe93633
Cory Laughlin <corylcomposinger@gmail.com>
2022-08-19 22:12:43 -0500

    Fleshed out event generator

--- tklife/event.py
+++ tklife/event.py
@@ -6,9 +6,32 @@ T_ActionCallable = Callable[[Event], Any]
 
 class EventsEnum(Enum):
     def generate(self, widget: Widget):
-        def generator(widget=widget):
+        def generator(*__, widget=widget):
             widget.event_generate(self.value)
         return generator
 
-    def bind(self, widget: Widget, action: T_ActionCallable):
-        widget.bind(self.value, action)
+    def bind(self, widget: Widget, action: T_ActionCallable, **kwargs):
+        return widget.bind(self.value, action, **kwargs)
+
+    def bind_all(self, widget: Widget, action: T_ActionCallable, **kwargs):
+        return widget.bind_all(self.value, action, **kwargs)
+
+    def bind_class(self, widget: Widget, classname: str, action: T_ActionCallable, **kwargs):
+        return widget.bind_class(classname, self.value, action, **kwargs)
+
+    def unbind(self, widget: Widget, funcid: str):
+        '''
+        See:
+            http://stackoverflow.com/questions/6433369/
+            deleting-and-changing-a-tkinter-event-binding-in-python
+            Modified
+        '''
+        if not funcid:
+            widget.tk.call('bind', widget._w, self.value, '')
+            return
+        func_callbacks = widget.tk.call(
+            'bind', widget._w, self.value, None).split('\n')
+        new_callbacks = [
+            l for l in func_callbacks if l[6:6 + len(funcid)] != funcid]
+        widget.tk.call('bind', widget._w, self.value, '\n'.join(new_callbacks))
+        widget.deletecommand(funcid)
