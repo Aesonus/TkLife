c13f386a6b45f024db0551ec2ea31d4cbe288379
Cory Laughlin <corylcomposinger@gmail.com>
2022-08-22 00:34:21 -0500

    Changed formatting

--- tklife/__main__.py
+++ tklife/__main__.py
@@ -1,6 +1,6 @@
 """Shows an example of a skeleton window"""
 
-from tkinter import E, W, Misc, StringVar, Tk, ttk
+from tkinter import BOTH, E, W, Misc, StringVar, Tk, ttk
 from tklife.constants import COMMAND, STICKY, TEXT, TEXTVARIABLE
 from tklife.controller import ControllerABC
 from tklife.skel import SkelWidget, SkeletonMixin
@@ -13,22 +13,32 @@ class ExampleController(ControllerABC):
     def button_b_command(self, *__):
         print(self.view.created['entry_b']['textvariable'].get())
 
+
 class ExampleView(SkeletonMixin, ttk.Frame):
-    def __init__(self, master: 'Misc', controller: ControllerABC, **kwargs) -> None:
+    def __init__(self, master: 'Misc', controller: ExampleController, **kwargs) -> None:
+        self.controller: ExampleController
         super().__init__(master, controller, **kwargs)
         self.created['entry_b']['textvariable'].set("Default value")
+
     @property
     def template(self):
         return (
-            [SkelWidget(ttk.Label, {TEXT: "Label A:"}, {}), SkelWidget(
-                ttk.Entry, {TEXTVARIABLE: StringVar}, {STICKY: E + W}, 'entry_a'), SkelWidget(ttk.Button, {TEXT: "Print contents", COMMAND: self.controller.button_a_command}, {})],
-            [SkelWidget(ttk.Label, {TEXT: "Label B:"}, {}), SkelWidget(
-                ttk.Entry, {TEXTVARIABLE: StringVar}, {STICKY: E + W}, 'entry_b'), SkelWidget(ttk.Button, {TEXT: "Print contents", COMMAND: self.controller.button_b_command}, {})],
+            [
+                SkelWidget(ttk.Label, {TEXT: "Label A:"}, {}),
+                SkelWidget(ttk.Entry, {TEXTVARIABLE: StringVar}, {STICKY: E + W}, 'entry_a'),
+                SkelWidget(ttk.Button, {TEXT: "Print contents", COMMAND: self.controller.button_a_command}, {})
+            ],
+            [
+                SkelWidget(ttk.Label, {TEXT: "Label B:"}, {}),
+                SkelWidget(ttk.Entry, {TEXTVARIABLE: StringVar}, {STICKY: E + W}, 'entry_b'),
+                SkelWidget(ttk.Button, {TEXT: "Print contents", COMMAND: self.controller.button_b_command}, {})
+            ],
         )
+
 
 if __name__ == "__main__":
     main = Tk()
     main.title("Skeleton Example")
     example_view = ExampleView(main, ExampleController())
-    example_view.pack(fill='both')
+    example_view.pack(fill=BOTH)
     main.mainloop()
