2de7e0231310b9185e78f90deb112807cba440ec
Cory Laughlin <corylcomposinger@gmail.com>
2022-08-23 12:31:52 -0500

    Added type hinting to ControllerABC

--- tklife/controller.py
+++ tklife/controller.py
@@ -1,5 +1,11 @@
+from typing import TYPE_CHECKING
 
+
+if TYPE_CHECKING:
+    import tkinter
+    from .skel import SkeletonMixin
+    from typing import Union
 
 class ControllerABC(object):
-    def set_view(self, view):
-        self.view = view
+    def set_view(self, view: 'Union[SkeletonMixin, tkinter.Misc]'):
+        self.view: 'Union[SkeletonMixin, tkinter.Misc]' = view
