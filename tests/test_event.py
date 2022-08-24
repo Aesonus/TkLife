8dc83a3da1789706829eb6f8be852c2a3701965e
Cory Laughlin <corylcomposinger@gmail.com>
2022-08-23 20:14:38 -0500

    Added more events and event modifiers

---
+++ tests/test_event.py
@@ -0,0 +1,98 @@
+from tkinter import Misc
+from types import SimpleNamespace
+import pytest
+
+from tklife.event import CompositeEvent, EventsEnum
+
+class TestEventEnum(object):
+    @pytest.fixture
+    def custom_event(self):
+        class Custom(EventsEnum):
+            TEST = "<<Test>>"
+        return Custom
+
+    @pytest.fixture
+    def action_callable(self):
+        return lambda __: None
+
+    @pytest.fixture
+    def mock_widget(self, mocker):
+        return mocker.Mock(Misc)
+
+    @pytest.mark.parametrize("kwargs", [
+        {}, {"test": True}
+    ], ids=["no kwargs", "with kwargs"])
+    def test_bind_calls(self, custom_event, action_callable, mock_widget, kwargs):
+        custom_event.TEST.bind(mock_widget, action_callable, **kwargs)
+        mock_widget.bind.assert_called_once_with(custom_event.TEST.value, action_callable, **kwargs)
+
+    @pytest.mark.parametrize("kwargs", [
+        {}, {"test": True}
+    ], ids=["no kwargs", "with kwargs"])
+    def test_bind_all_calls(self, custom_event, action_callable, mock_widget, kwargs):
+        custom_event.TEST.bind_all(mock_widget, action_callable, **kwargs)
+        mock_widget.bind_all.assert_called_once_with(custom_event.TEST.value, action_callable, **kwargs)
+
+    @pytest.mark.parametrize("kwargs", [
+        {}, {"test": True}
+    ], ids=["no kwargs", "with kwargs"])
+    def test_bind_class_calls(self, custom_event, action_callable, mock_widget, kwargs):
+        custom_event.TEST.bind_class(mock_widget, "classname", action_callable, **kwargs)
+        mock_widget.bind_class.assert_called_once_with("classname", custom_event.TEST.value, action_callable, **kwargs)
+
+    def test_generate_returns_callable_that_calls_event_generate(
+        self, custom_event, mock_widget
+    ):
+        callable_ = custom_event.TEST.generate(mock_widget)
+        callable_()
+        mock_widget.event_generate.assert_called_once_with(custom_event.TEST.value)
+
+
+class TestCompositeEvent(object):
+    @pytest.fixture
+    def composite_event(self):
+        return CompositeEvent("<<Test>>")
+
+    @pytest.fixture
+    def action_callable(self):
+        return lambda __: None
+
+    @pytest.fixture
+    def mock_widget(self, mocker):
+        return mocker.Mock(Misc)
+
+    def test_composite_event_factory(self):
+        expected = "<Mod-Event>"
+        actual = CompositeEvent.factory(SimpleNamespace(value="<Mod>"), SimpleNamespace(value="<Event>"))
+        assert actual.value == expected
+
+    @pytest.mark.parametrize("kwargs", [
+        {}, {"test": True}
+    ], ids=["no kwargs", "with kwargs"])
+    def test_bind_calls(self, composite_event, action_callable, mock_widget, kwargs):
+        composite_event.bind(mock_widget, action_callable, **kwargs)
+        mock_widget.bind.assert_called_once_with(composite_event.value, action_callable, **kwargs)
+
+    @pytest.mark.parametrize("kwargs", [
+        {}, {"test": True}
+    ], ids=["no kwargs", "with kwargs"])
+    def test_bind_all_calls(self, composite_event, action_callable, mock_widget, kwargs):
+        composite_event.bind_all(mock_widget, action_callable, **kwargs)
+        mock_widget.bind_all.assert_called_once_with(composite_event.value, action_callable, **kwargs)
+
+    @pytest.mark.parametrize("kwargs", [
+        {}, {"test": True}
+    ], ids=["no kwargs", "with kwargs"])
+    def test_bind_class_calls(self, composite_event, action_callable, mock_widget, kwargs):
+        composite_event.bind_class(mock_widget, "classname", action_callable, **kwargs)
+        mock_widget.bind_class.assert_called_once_with("classname", composite_event.value, action_callable, **kwargs)
+
+    def test_generate_returns_callable_that_calls_event_generate(
+        self, composite_event, mock_widget
+    ):
+        callable_ = composite_event.generate(mock_widget)
+        callable_()
+        mock_widget.event_generate.assert_called_once_with(composite_event.value)
+
+def test_event_addition():
+    pass
--- tklife/event.py
+++ tklife/event.py
@@ -1,12 +1,12 @@
 from enum import Enum
 from tkinter import Event, Widget
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 
 T_ActionCallable = Callable[[Event], Any]
 
 
-class EventsEnum(Enum):
-    def generate(self, widget: Widget):
+class _EventMixin(object):
+    def generate(self, widget: Widget) -> T_ActionCallable:
         def generator(*__, widget=widget):
             widget.event_generate(self.value)
         return generator
@@ -20,7 +20,7 @@ class EventsEnum(Enum):
     def bind_class(self, widget: Widget, classname: str, action: T_ActionCallable, **kwargs):
         return widget.bind_class(classname, self.value, action, **kwargs)
 
-    def unbind(self, widget: Widget, funcid: str):
+    def unbind(self, widget: Widget, funcid: Optional[str] = None):
         '''
         See:
             http://stackoverflow.com/questions/6433369/
@@ -36,3 +36,113 @@ class EventsEnum(Enum):
             l for l in func_callbacks if l[6:6 + len(funcid)] != funcid]
         widget.tk.call('bind', widget._w, self.value, '\n'.join(new_callbacks))
         widget.deletecommand(funcid)
+
+    def __add__(self, arg):
+        return CompositeEvent.factory(self, arg)
+
+
+class EventsEnum(_EventMixin, Enum):
+    pass
+
+
+class CompositeEvent(_EventMixin):
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    @classmethod
+    def factory(cls, modifier, event) -> 'CompositeEvent':
+        return cls(f"{modifier.value[0:-1]}-{event.value[1:]}")
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.value})"
+
+
+class TkEventMod(EventsEnum):
+    ALT = "<Alt>"
+    ANY = "<Any>"
+    CONTROL = "<Control>"
+    DOUBLE = "<Double>"
+    LOCK = "<Lock>"
+    SHIFT = "<Shift>"
+    TRIPLE = "<Triple>"
+
+
+class TkEvent(EventsEnum):
+
+    ACTIVATE = "<Activate>"
+    BUTTON = "<Button>"
+    BUTTONRELEASE = "<ButtonRelease>"
+    CONFIGURE = "<Configure>"
+    DEACTIVATE = "<Deactivate>"
+    DESTROY = "<Destroy>"
+    ENTER = "<Enter>"
+    EXPOSE = "<Expose>"
+    FOCUSIN = "<FocusIn>"
+    FOCUSOUT = "<FocusOut>"
+    KEYPRESS = "<KeyPress>"
+    KEYRELEASE = "<KeyRelease>"
+    LEAVE = "<Leave>"
+    MAP = "<Map>"
+    MOTION = "<Motion>"
+    MOUSEWHEEL = "<MouseWheel>"
+    UNMAP = "<Unmap>"
+    VISIBILITY = "<Visibility>"
+    ALT_L = "<Alt_L>"
+    ALT_R = "<Alt_R>"
+    BACKSPACE = "<BackSpace>"
+    CANCEL = "<Cancel>"
+    CAPS_LOCK = "<Caps_Lock>"
+    CONTROL_L = "<Control_L>"
+    CONTROL_R = "<Control_R>"
+    DELETE = "<Delete>"
+    DOWN = "<Down>"
+    END = "<End>"
+    ESCAPE = "<Escape>"
+    EXECUTE = "<Execute>"
+    F1 = "<F1>"
+    F2 = "<F2>"
+    FI = "<Fi>"
+    F12 = "<F12>"
+    HOME = "<Home>"
+    INSERT = "<Insert>"
+    LEFT = "<Left>"
+    LINEFEED = "<Linefeed>"
+    KP_0 = "<KP_0>"
+    KP_1 = "<KP_1>"
+    KP_2 = "<KP_2>"
+    KP_3 = "<KP_3>"
+    KP_4 = "<KP_4>"
+    KP_5 = "<KP_5>"
+    KP_6 = "<KP_6>"
+    KP_7 = "<KP_7>"
+    KP_8 = "<KP_8>"
+    KP_9 = "<KP_9>"
+    KP_ADD = "<KP_Add>"
+    KP_BEGIN = "<KP_Begin>"
+    KP_DECIMAL = "<KP_Decimal>"
+    KP_DELETE = "<KP_Delete>"
+    KP_DIVIDE = "<KP_Divide>"
+    KP_DOWN = "<KP_Down>"
+    KP_END = "<KP_End>"
+    KP_ENTER = "<KP_Enter>"
+    KP_HOME = "<KP_Home>"
+    KP_INSERT = "<KP_Insert>"
+    KP_LEFT = "<KP_Left>"
+    KP_MULTIPLY = "<KP_Multiply>"
+    KP_NEXT = "<KP_Next>"
+    KP_PRIOR = "<KP_Prior>"
+    KP_RIGHT = "<KP_Right>"
+    KP_SUBTRACT = "<KP_Subtract>"
+    KP_UP = "<KP_Up>"
+    NEXT = "<Next>"
+    NUM_LOCK = "<Num_Lock>"
+    PAUSE = "<Pause>"
+    PRINT = "<Print>"
+    PRIOR = "<Prior>"
+    RETURN = "<Return>"
+    RIGHT = "<Right>"
+    SCROLL_LOCK = "<Scroll_Lock>"
+    SHIFT_L = "<Shift_L>"
+    SHIFT_R = "<Shift_R>"
+    TAB = "<Tab>"
+    UP = "<Up>"
