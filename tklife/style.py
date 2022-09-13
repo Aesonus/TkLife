d4afd90b1f061ce31f29e1ebd256ead6658398b8
Cory Laughlin <claughlin@fieldsfoods.com>
2022-09-13 09:38:39 -0500

    In progress of adding style definitions as Enums

---
+++ tklife/style.py
@@ -0,0 +1,20 @@
+import enum
+
+
+class StyleEnum(enum.Enum):
+    """Holds the definition of styles"""
+
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def tkstyle_name(self):
+        pass
+
+    @property
+    def normal_styles(self):
+        pass
+
+    @property
+    def dynamic_styles(self):
+        pass
