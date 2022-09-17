c49efc5ee2e598611068e2c77a6a080462c94ce3
Cory Laughlin <corylcomposinger@gmail.com>
2022-09-16 21:51:32 -0500

    Add more style. WIP

--- tests/style/test_style.py
+++ tests/style/test_style.py
@@ -1,12 +1,14 @@
 import pytest
 from tklife.style.style import TEntry
+from pytest_mock import MockerFixture
 
+parametrize = pytest.mark.parametrize
 class TestBaseStyle:
-    @pytest.mark.parametrize("subclass, expected", [
+    @parametrize("subclass, expected", [
         (type("TestEntry", (TEntry,), {}), "TestEntry.TEntry"),
         (type("HigherLevel", (type("TestEntry", (TEntry,), {}),), {}), "HigherLevel.TestEntry.TEntry"),
     ])
-    def test_ttk_style_returns_ttk_style_name_based_on_inheritance_structure(
+    def test_tkl_style_returns_ttk_style_name_based_on_inheritance_structure(
         self, subclass, expected
     ):
         assert subclass.ttk_style == expected
--- tklife/style/style.py
+++ tklife/style/style.py
@@ -1,16 +1,43 @@
+from typing import Any
+from collections.abc import Sequence
+import abc
+from tkinter.ttk import Style
 
-
-class _StyleMeta(type):
-
-    def __new__(cls, name, bases, namespace):
-        return super().__new__(cls, name, bases, namespace)
+class _StyleMeta(abc.ABCMeta):
 
     @property
     def ttk_style(cls):
-        return ".".join((cls.__name__, *(b.__name__ for b in cls.__bases__)))
+        def iter_bases(__bases):
+            for base in __bases:
+                if base == BaseStyle:
+                    continue
+                yield base
+                for base2 in iter_bases(base.__bases__):
+                    yield base2
+        if not hasattr(cls, "__name_cache__"):
+            cls.__name_cache__ = ".".join((cls.__name__, *(b.__name__ for b in iter_bases(cls.__bases__))))
+        return cls.__name_cache__
 
 class BaseStyle(metaclass=_StyleMeta):
     """All the base styles inherit from this class"""
 
 class TEntry(BaseStyle):
     """This would be the base style. Extending this would add a new style type"""
+    bordercolor: str
+    darkcolor: str
+    fieldbackground: str
+    foreground: str
+    insertcolor: str
+    insertwidth: int
+    lightcolor: str
+    padding: int
+    relief: str
+    selectbackground: str
+    selectborderwidth: int
+    selectforeground: str
+
+class Example(TEntry):
+    padding = {
+        ("normal", 3),
+        ("readonly", 4, 3),
+    }
