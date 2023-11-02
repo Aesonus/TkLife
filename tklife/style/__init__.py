ce575749b00f0aab6d39e023432b32edae5a8637
Cory Laughlin <corylcomposinger@gmail.com>
2023-11-01 19:52:38 -0500

    Import style in package initializer.
    Version bump.

---
+++ tklife/style/__init__.py
@@ -0,0 +1,11 @@
+"""This module contains the BaseStyle class, which is the base class for all styles.
+
+It also contains all the styles that are defined by default, and the _StyleMeta class,
+which is a metaclass for BaseStyle that automatically registers all classes that inherit
+from it. This allows for easy access to the Ttk Style name, and configuration and map
+options.
+
+"""
+from . import style
+
+__all__ = ["style"]
