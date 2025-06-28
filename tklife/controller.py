300e8ff2ea405c9ec453087d6b2e6548d4437433
Cory Laughlin <corylcomposinger@gmail.com>
2025-06-28 16:09:18 -0500

    refactor(controller): use absolute import statements

--- tklife/controller.py
+++ tklife/controller.py
@@ -5,8 +5,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from . import CreatedWidget
-    from .core import SkeletonProtocol
+    from tklife.core import CreatedWidget, SkeletonProtocol
 
 
 class ControllerABC:
