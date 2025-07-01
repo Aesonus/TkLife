f8c78b05ae441de95cc4b7e75d1d52ad81d76646
Cory Laughlin <corylcomposinger@gmail.com>
2025-07-01 09:03:24 -0500

    chore(release): bump project version to `2.5.0.dev1`

--- tklife/__init__.py
+++ tklife/__init__.py
@@ -3,6 +3,6 @@
 from tklife import constants, controller, core, event, menu  # noqa: F401
 from tklife.core import *  # noqa: F401
 
-__version__ = "2.5.0-dev0"
+__version__ = "2.5.0-dev1"
 
 __all__ = ["constants", "controller", "core", "event", "menu"] + core.__all__
