aa7bfe9b2c6a3c8e5929aacb5cd0ab6f27de73ad
Cory Laughlin <corylcomposinger@gmail.com>
2025-07-20 09:51:14 -0500

    refactor(widgets): update geometry method copying in `ScrolledListbox`
    Replace geometry method copying with a call to `copy_geometry_methods` in the
    `ScrolledListbox` class constructor.

--- tklife/widgets.py
+++ tklife/widgets.py
@@ -281,15 +281,7 @@ class ScrolledListbox(tk.Listbox):
         self.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.vbar["command"] = self.yview
 
-        # Copy geometry methods of self.frame without overriding Listbox
-        # methods -- hack!
-        text_meths = vars(tk.Listbox).keys()
-        methods = vars(tk.Pack).keys() | vars(tk.Grid).keys() | vars(tk.Place).keys()
-        methods = methods.difference(text_meths)
-
-        for m in methods:
-            if m[0] != "_" and m != "config" and m != "configure":
-                setattr(self, m, getattr(self.frame, m))
+        copy_geometry_methods(self.frame, self)
 
     def __str__(self):
         return str(self.frame)
